216335de2fcadfc2b3b96d0084120eb55983ba2d
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2025-11-18 14:25:34 -0800

    style(ui): make footer page content responsive on mobile while maintaining 75vw width on larger screens

--- src/app/pages/Privacy.tsx
+++ src/app/pages/Privacy.tsx
@@ -1,48 +1,48 @@
 export default function Privacy() {
   return (
-    <div className="flex items-center justify-center ">
-      <div className="flex flex-col flex-wrap items-start w-[60vw] space-y-4">
-        <h1 className="font-bold text-[4rem] text-gray-900 leading-[1] mb-4">Privacy Policy</h1>
-        <p className="text-[1.2rem] text-gray-900">
+    <div className='flex items-center justify-center '>
+      <div className='flex flex-col flex-wrap items-start w-full md:w-[75vw] max-w-screen-md px-6 space-y-4 break-words'>
+        <h1 className='font-bold text-[4rem] text-gray-900 leading-[1] mb-4'>Privacy Policy</h1>
+        <p className='text-[1.2rem] text-gray-900'>
           The Berkeley Mobile iOS app is created by the Associated Students of the University of
           California through the Office of the CTO. If you would like a copy of the Berkeley Mobile
           Privacy Policy, please email{" "}
           <a
-            href="mailto:octo.berkeleymobile@asuc.org"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-600 cursor-pointer inline-block transition-all duration-300 ease-in-out hover:-translate-y-1"
+            href='mailto:octo.berkeleymobile@asuc.org'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='text-blue-600 cursor-pointer inline-block transition-all duration-300 ease-in-out hover:-translate-y-1'
           >
             octo.berkeleymobile@asuc.org
           </a>
           .
         </p>
-        <h2 className="text-xl font-semibold mt-4">Data Usage</h2>
-        <p className="text-[1.2rem] text-gray-900">
+        <h2 className='text-xl font-semibold mt-4'>Data Usage</h2>
+        <p className='text-[1.2rem] text-gray-900'>
           We use precise location data only if users enable their location on their iOS device. Your
           location will be used to calculate distance to points of interest, display your location
           on the map, and for other location-based features. We do not collect or retain these data.
           Users can disable location usage for this app within their device's settings.
         </p>
-        <p className="text-[1.2rem] text-gray-900">
+        <p className='text-[1.2rem] text-gray-900'>
           Calendar access will only be used to allow you to add events you select to your calendar.
           We do not collect or retain these data. We request user permission each time this feature
           is used.
         </p>
 
-        <h2 className="text-xl font-semibold mt-4">Collected Data</h2>
-        <p className="text-[1.2rem] text-gray-900">
+        <h2 className='text-xl font-semibold mt-4'>Collected Data</h2>
+        <p className='text-[1.2rem] text-gray-900'>
           Collected data is used for analytics or diagnostics (e.g. analysis of feature adoption;
           understanding and resolving app crashes). We do not use Apple's Identifier for Advertisers
           (IDFA), and collected data are not tracked or associated with advertising profiles.
         </p>
-        <p className="text-[1.2rem] text-gray-900">
+        <p className='text-[1.2rem] text-gray-900'>
           We use{" "}
           <a
-            href="https://firebase.google.com/products/analytics"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-600 cursor-pointer inline-block transition-all duration-300 ease-in-out hover:-translate-y-1"
+            href='https://firebase.google.com/products/analytics'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='text-blue-600 cursor-pointer inline-block transition-all duration-300 ease-in-out hover:-translate-y-1'
           >
             Google Analytics for Firebase
           </a>{" "}
@@ -52,10 +52,10 @@ export default function Privacy() {
           instance identifier. Demographic information (e.g. age, gender, etc.) are not used or
           collected. Read more about Google's collection policies at{" "}
           <a
-            href="https://firebase.google.com/support/privacy#data_processing_information"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-600 cursor-pointer inline-block transition-all duration-300 ease-in-out hover:-translate-y-1"
+            href='https://firebase.google.com/support/privacy#data_processing_information'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='text-blue-600 cursor-pointer inline-block transition-all duration-300 ease-in-out hover:-translate-y-1'
           >
             this link.
           </a>
--- src/app/pages/ReleaseNotes.tsx
+++ src/app/pages/ReleaseNotes.tsx
@@ -3,9 +3,9 @@ import { notes } from "../../data/release-notes";
 
 export default function ReleaseNotes() {
   return (
-    <div className="flex items-center justify-center ">
-      <div className="flex flex-col flex-wrap items-start w-[60vw]">
-        <h1 className="font-bold text-[4rem] text-gray-900 leading-[1] mb-4">Release Notes</h1>
+    <div className='flex items-center justify-center '>
+      <div className='flex flex-col flex-wrap items-start w-full md:w-[75vw] max-w-screen-md px-6 space-y-4 break-words'>
+        <h1 className='font-bold text-[4rem] text-gray-900 leading-[1] mb-4'>Release Notes</h1>
         {notes.map((note) => (
           <VersionEntry
             key={note.version}
